d1e1dfd832a5351b9c09325c247f1189f2cf3fff
ElRecipiente <nicolas.droux.2b@gmail.com>
2023-01-18 10:42:24 +0100

    P4 fixed and playable

--- P4.js
+++ P4.js
@@ -4,57 +4,30 @@ let logger = document.querySelector("div.log");
 let whichTurn = false;
 let gameEnd = false;
 
-// We want (database.length == 25) here
+// We want (database.length == 36) here
 for (i = 0; i < 35; i++) {
     database.push("");
     console.log(database.length)
 }
 
+// reload
 function reload() {
     location.reload();
 }
 
+// IA rand
 function rand(i) {
     return Math.ceil(Math.random() * (i - 1))
 }
 
+// show result in html
 function display() {
     for (let i = 0; i < grid.length; i++) {
         grid[i].textContent = database[i];
     }
 }
 
-// function play(x) {
-//     if (gameEnd) {
-//         logger.innerHTML += `<br>Click on "Play again !" if you want to... play again.`;
-//     }
-//     else if ((whichTurn == false) && (database[x] == "X" || database[x] == "O")) {
-//         logger.textContent = "Already played, choose another cell.";
-//     }
-//     else {
-//         if (whichTurn) {
-//             if (database[x] == "X" || database[x] == "O") {
-//                 play(rand(6));
-//             }
-//             else {
-//                 fall(x);
-//                 display();
-//                 // whoWin();
-//                 whichTurn = false;
-//             }
-//         }
-//         else {
-//             fall(x);
-//             display();
-//             // whoWin();
-//             whichTurn = true;
-//             setTimeout(() => {
-//                 play(rand(6))
-//             }, 100);;
-//         }
-//     }
-// }
-
+//tell in log when a column is full
 function full(y) {
     if (database[y % 6] == "X" || database[y % 6] == "O") {
         return true;
@@ -64,52 +37,91 @@ function full(y) {
     }
 }
 
-function fallX(y) {
-    console.log("fall : " + y)
-    console.log("db y : " + database[y])
-
-    if (full(y)) {
-        logger.textContent = "Column full, please try another."
+// made symbol "fall" in the grid, like a Puissance 4
+function fall(y) {
+    if (gameEnd) {
+        logger.innerHTML += `<br>Click on "Play again !" if you want to... play again.`;
     }
-    else if ((database[y] != "X" && database[y] != "O") && y <= 30) {
-        console.log("case courante dispo, follwing")
-        fallX(y + 6);
+    else if (whichTurn) {
+        // here play "O"
+        if (full(y)) {
+            logger.textContent = "Column full, please try another."
+        }
+        else if ((database[y] != "X" && database[y] != "O") && y <= 30) {
+            console.log("case courante dispo, following")
+            fall(y + 6);
+        }
+        else if (database[y] == "X" || database[y] == "O" || y > 35) {
+            console.log("case courante pas dispo, placement au precedent")
+            database[y - 6] = "O";
+            display();
+            whoWin();
+        }
+        else {
+            console.log("coucou je suis else")
+            database[y] = "O"
+            display();
+            whoWin();
+        }
+        whichTurn = false;
     }
-    else if (database[y] == "X" || database[y] == "O" || y > 35) {
-        console.log("case courante pas dispo, placement au precedent")
-        database[y - 6] = "X";
-        display()
-    }
+    // here play "X"
     else {
-        console.log("heoy")
-        database[y] = "X"
-        display();
+        if (full(y)) {
+            logger.textContent = "Column full, please try another."
+        }
+        else if ((database[y] != "X" && database[y] != "O") && y <= 30) {
+            console.log("case courante dispo, following")
+            fall(y + 6);
+        }
+        else if (database[y] == "X" || database[y] == "O" || y > 35) {
+            console.log("case courante pas dispo, placement au precedent")
+            database[y - 6] = "X";
+            display();
+            whoWin();
+        }
+        else {
+            console.log("coucou je suis else")
+            database[y] = "X"
+            display();
+            whoWin();
+        }
+        whichTurn = true;
     }
 }
 
+//tell in log when a player win
 function whoWin() {
     for (let j = 0; j < 2; j++) {
         p = (j == 0 ? "X" : "O")
+        // for columns
+        for (r = 0; r < database.length; r = r + 6) {
+            if ((database[r] == p && database[r + 1] == p && database[r + 2] == p && database[r + 3] == p)
+                || (database[r + 1] == p && database[r + 2] == p && database[r + 3] == p && database[r + 4] == p)
+                || (database[r + 2] == p && database[r + 3] == p && database[r + 4] == p && database[r + 5] == p)) {
+                logger.textContent = `Player ${p} wins !`;
+                gameEnd = true;
+            }
+        }
 
-        for (r = 0; r < database.length; r = r + 3) {
-            if (database[r] == p && database[r + 1] == p && database[r + 2] == p) {
-                logger.textContent = `Player ${p} wins !`;
-                gameEnd = true;
-            }
-        }
+        //  for rows
         for (c = 0; c < database.length; c++) {
-            if (database[c] == p && database[c + 3] == p && database[c + 6] == p) {
+            if (database[c] == p && database[c + 6] == p && database[c + 12] == p && database[c + 18] == p) {
                 logger.textContent = `Player ${p} wins !`;
                 gameEnd = true;
             }
-        }
-        if (database[0] == p && database[4] == p && database[8] == p) {
-            logger.textContent = `Player ${p} wins !`;
-            gameEnd = true;
-        }
-        else if (database[2] == p && database[4] == p && database[6] == p) {
-            logger.textContent = `Player ${p} wins !`;
-            gameEnd = true;
+
+            //and diags
+            else if (((c <= 2 && c >= 6) || (c <= 8 && c >= 12))
+                && database[c] == p && database[c + 7] == p && database[c + 14] == p && database[c + 21] == p) {
+                logger.textContent = `Player ${p} wins !`;
+                gameEnd = true;
+            }
+            else if (((c >= 3 && c <= 5) || (c >= 9 && c <= 11) || (c >= 15 && c <= 17))
+                && database[c] == p && database[c + 5] == p && database[c + 10] == p && database[c + 15] == p) {
+                logger.textContent = `Player ${p} wins !`;
+                gameEnd = true;
+            }
         }
     }
 }
