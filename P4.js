adb06d199ee66c65f8f065ab0cffac171b1cac27
ElRecipiente <nicolas.droux.2b@gmail.com>
2023-01-20 10:59:18 +0100

    reup of all previous commit lost (animated background, music and some fix js)

--- P4.js
+++ P4.js
@@ -5,7 +5,7 @@ let logger = document.querySelector("div.log");
 let whichTurn = false;
 let gameEnd = false;
 
-logger.textContent = "Welcome to P4 !"
+logger.innerHTML = `<p>Player ONE is orange.<br>Click to the top of the grid to start the game.<p>`
 
 // We want (database.length == 36) here
 for (i = 0; i < 35; i++) {
@@ -84,8 +84,8 @@ function fall(y) {
             setTimeout(() => {
                 grid[y].classList.remove("blueFall");
                 fall(y + 6);
-            },300)
-            ;
+            }, 200)
+                ;
         }
         else if (database[y] == "X" || database[y] == "O" || y > 35) {
             console.log("case courante pas dispo, placement au precedent")
@@ -116,8 +116,8 @@ function fall(y) {
             setTimeout(() => {
                 grid[y].classList.remove("redFall");
                 fall(y + 6);
-            },300)
-            
+            }, 200)
+
         }
         else if (database[y] == "X" || database[y] == "O" || y > 35) {
             console.log("case courante pas dispo, placement au precedent")
@@ -151,7 +151,7 @@ function whoWin() {
             if ((database[r] == p && database[r + 1] == p && database[r + 2] == p && database[r + 3] == p)
                 || (database[r + 1] == p && database[r + 2] == p && database[r + 3] == p && database[r + 4] == p)
                 || (database[r + 2] == p && database[r + 3] == p && database[r + 4] == p && database[r + 5] == p)) {
-                logger.innerHTML += `<br>Player ${p} wins !`;
+                logger.innerHTML = `<br><p id="color">Player ${p == "X" ? "ONE" : "TWO"} wins !</p><img src="img/thunder.gif" alt="thunder" width="100%"><audio src="music/thunderSound.mp3" autoplay></audio>`;
                 gameEnd = true;
             }
         }
@@ -159,19 +159,19 @@ function whoWin() {
         //  for rows
         for (c = 0; c < database.length; c++) {
             if (database[c] == p && database[c + 6] == p && database[c + 12] == p && database[c + 18] == p) {
-                logger.innerHTML += `<br>Player ${p} wins !`;
+                logger.innerHTML = `<br><p id="color">Player ${p == "X" ? "ONE" : "TWO"} wins !</p><img src="img/thunder.gif" alt="thunder" width="100%"><audio src="music/thunderSound.mp3" autoplay></audio>`;
                 gameEnd = true;
             }
 
             //and diags
             else if (((c <= 2) || (c >= 6 && c <= 8) || (c >= 12))
                 && database[c] == p && database[c + 7] == p && database[c + 14] == p && database[c + 21] == p) {
-                logger.innerHTML += `<br>Player ${p} wins !`;
+                logger.innerHTML = `<br><p id="color">Player ${p == "X" ? "ONE" : "TWO"} wins !</p><img src="img/thunder.gif" alt="thunder" width="100%"><audio src="music/thunderSound.mp3" autoplay></audio>`;
                 gameEnd = true;
             }
             else if (((c >= 3 && c <= 5) || (c >= 9 && c <= 11) || (c >= 15 && c <= 17))
                 && database[c] == p && database[c + 5] == p && database[c + 10] == p && database[c + 15] == p) {
-                logger.innerHTML += `<br>Player ${p} wins !`;
+                logger.innerHTML = `<br><p id="color">Player ${p == "X" ? "ONE" : "TWO"} wins !</p><img src="img/thunder.gif" alt="thunder" width="100%"><audio src="music/thunderSound.mp3" autoplay></audio>`;
                 gameEnd = true;
             }
         }
